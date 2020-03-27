617e0e751b992bd4d38f7aba3d4c692559ec8d54
Peita Lin <n6378056@gmail.com>
2020-03-27 19:28:22 +1000

    try inject google login callbackscript

--- layout/Header.tsx
+++ layout/Header.tsx
@@ -46,12 +46,29 @@ const Header: React.FC<{}> = (props) => (
 
 export const getGoogleLoginScript = (): string => {
   return `
+    function onSignIn(googleUser) {
+      // Useful data for your client-side scripts:
+      var profile = googleUser.getBasicProfile();
+      console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+      console.log('Full Name: ' + profile.getName());
+      console.log('Given Name: ' + profile.getGivenName());
+      console.log('Family Name: ' + profile.getFamilyName());
+      console.log("Image URL: " + profile.getImageUrl());
+      console.log("Email: " + profile.getEmail());
+
+      // The ID token you need to pass to your backend:
+      var id_token = googleUser.getAuthResponse().id_token;
+      console.log("ID Token: " + id_token);
+    }
+
     function onSuccess(googleUser) {
       console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
     }
+
     function onFailure(error) {
       console.log(error);
     }
+
     function renderButton() {
       gapi.signin2.render('my-signin2', {
         'scope': 'profile email',
