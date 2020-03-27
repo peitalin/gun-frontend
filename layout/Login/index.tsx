4f243b8a2ba63ba0213b0e08e1054524cac7dac1
Peita Lin <n6378056@gmail.com>
2020-03-27 19:14:04 +1000

    callbacks for google login test

--- layout/Login/index.tsx
+++ layout/Login/index.tsx
@@ -15,39 +15,20 @@ const Login: React.FC<ReactProps> = (props) => {
 
   const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
-  const signOut = () => {
-    var auth2 = window.gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-  }
-
-  const onSignIn = (googleUser) => {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  }
-
   if (isAuthenticated) {
     return (
-      <>
-        <a href="#" onClick={signOut}>Sign out</a>
-        <a href="#" onClick={onSignIn}>On Sign In</a>
-        <Button
-          variant="text"
-          color="secondary"
-          className={classes.loginButton}
-          onClick={() => {
-            logout()
-            props.callbackOnComplete()
-          }}
-          {...props.buttonProps}
-        >
-          { props.logoutTitle ? props.logoutTitle : "Logout" }
-        </Button>
-      </>
+      <Button
+        variant="text"
+        color="secondary"
+        className={classes.loginButton}
+        onClick={() => {
+          logout()
+          props.callbackOnComplete()
+        }}
+        {...props.buttonProps}
+      >
+        { props.logoutTitle ? props.logoutTitle : "Logout" }
+      </Button>
     )
   } else {
     return (
--- layout/NavBarMain/MainBar/index.tsx
+++ layout/NavBarMain/MainBar/index.tsx
@@ -71,11 +71,25 @@ const MainBar = (props: ReactProps) => {
     subtotal,
   };
 
+  const signOut = () => {
+    var auth2 = window.gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
+
+  const onSignIn = (googleUser) => {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+
   return (
     <nav className={clsx(
       classes.baseBar,
     )}>
-      <div className="g-signin2" data-onsuccess="onSignIn"></div>
 
       {/* MOBILE */}
       <Hidden smUp implementation="css">
@@ -112,6 +126,10 @@ const MainBar = (props: ReactProps) => {
           {...navBarProps}
         />
       </Hidden>
+
+      <div className="g-signin2" data-onsuccess="onSignIn"></div>
+      <a href="#" onClick={signOut}>Sign out</a>
+      <a href="#" onClick={onSignIn}>On Sign In</a>
 
     </nav>
   );
