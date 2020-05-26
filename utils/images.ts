ea41ce6eadcba07bf038a5ac462331d3c52c8f62
Peita Lin <n6378056@gmail.com>
2020-05-26 17:13:06 +1000

    utils/images

---
+++ utils/images.ts
@@ -0,0 +1,33 @@
+import { ID, Image, Image_Parents } from "typings/gqlTypes";
+import { Breakpoints } from "layout/AppTheme";
+
+export const concatChunksToUInt8Array = (chunks, receivedLength): Uint8Array => {
+  // See: https://javascript.info/fetch-progress
+  let chunksAll = new Uint8Array(receivedLength); // (4.1)
+  let position = 0;
+  // interate and set each chunk in the correct position
+  for (let chunk of chunks) {
+    chunksAll.set(chunk, position);
+    position += chunk.length;
+  }
+  return chunksAll
+}
+
+
+// https://developer.mozilla.org/en-US/docs/Learn/HTML/Multimedia_and_embedding/Responsive_images
+export const genSrcSet = (image: Image_Parents) => {
+  return image.variants.map(v => {
+    return `${v.url} ${v.widthInPixels}w`
+  }).join(",")
+}
+
+export const genImgBreakpoints = ({ xs, sm, md, lg, xl }: Breakpoints) => {
+  return `
+  (max-width: 416px) ${xs}px,
+  (max-width: 480px) ${sm}px,
+  (max-width: 720px) ${md}px,
+  (max-width: 900px) ${lg}px,
+  (min-width: 901px) ${xl}px,
+  `
+}
+
