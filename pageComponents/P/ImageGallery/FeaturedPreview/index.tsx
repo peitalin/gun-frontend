0d1f1a45e25a6abe8c18c2fbf3191e8fedfd9bdb
Peita Lin <n6378056@gmail.com>
2021-08-06 14:27:16 +1000

    remove fill-rule background

--- pageComponents/P/ImageGallery/FeaturedPreview/index.tsx
+++ pageComponents/P/ImageGallery/FeaturedPreview/index.tsx
@@ -71,8 +71,6 @@ const styles = (theme: Theme) => createStyles({
     height: '100%',
     display: "flex",
     flexDirection: "row",
-    // backgroundColor: backgroundColor,
-    // backgroundImage: `url("data:image/svg+xml,%3Csvg width='20' height='20' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M20 20.5V18H0v-2h20v-2H0v-2h20v-2H0V8h20V6H0V4h20V2H0V0h22v20h2V0h2v20h2V0h2v20h2V0h2v20h2V0h2v20h2v2H20v-1.5zM0 20h2v20H0V20zm4 0h2v20H4V20zm4 0h2v20H8V20zm4 0h2v20h-2V20zm4 0h2v20h-2V20zm4 4h20v2H20v-2zm0 4h20v2H20v-2zm0 4h20v2H20v-2zm0 4h20v2H20v-2z' fill='%23${patternColor}' fill-opacity='0.3' fill-rule='evenodd'/%3E%3C/svg%3E")`,
   },
   cardActionAreaTall: {
     height: '100%',
