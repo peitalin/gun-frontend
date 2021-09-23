de1848d9eb75f030409a86e87c2b2d61373bdf14
Peita Lin <n6378056@gmail.com>
2021-09-23 14:14:48 +1000

    storeUserId

--- pageComponents/P/ProductId.tsx
+++ pageComponents/P/ProductId.tsx
@@ -194,7 +194,8 @@ const Products: React.FC<ReactProps> = (props) => {
     return <ErrorPage statusCode={403} message={"Product is not published"}/>
   }
   if (!loading && !productIsYours && storeUserVerified !== true) {
-    return <ErrorPage statusCode={400} message={`Store's owner ${product?.storeId} has yet to be verified`}/>
+    let storeOwnerId = product?.store?.userId ?? product?.store?.user?.id
+    return <ErrorPage statusCode={400} message={`Store's owner ${storeOwnerId} has yet to be verified`}/>
   }
   if (error) {
     return <ErrorPage statusCode={404} message={"Product cannot be found"}/>
