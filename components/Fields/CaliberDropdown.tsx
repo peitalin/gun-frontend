8d56210ccba926e844ad9b2c76a0628393e608de
Peita Lin <n6378056@gmail.com>
2021-12-17 15:01:04 +1000

    new caliber dropdown

---
+++ components/Fields/CaliberDropdown.tsx
@@ -0,0 +1,267 @@
+import React from "react";
+import clsx from "clsx";
+import { withStyles, WithStyles } from "@material-ui/core/styles";
+// Select Component
+import Select from "react-select";
+
+import { Colors, Gradients, fontFam, BorderRadius, isThemeDark } from "layout/AppTheme";
+import { useTheme } from "@material-ui/core/styles";
+import { useFocus } from "utils/hooks";
+import { styles } from "components/Fields/styles";
+import { useSnackbar } from "notistack"
+
+
+
+const CaliberDropdown = (props: ReactProps) => {
+
+  const ref = React.useRef(null);
+  const focused = useFocus(ref);
+
+  const theme = useTheme();
+  const isDarkMode = isThemeDark(theme)
+
+  const {
+    // Formik
+    errorMessage,
+    touched = false,
+    disableInitialValidationMessage = false,
+    // CreatableSelect
+    creatable = false,
+    isMulti = false,
+    isSearchable = true,
+    isClearable = false,
+    height = 40,
+    limit,
+    classes,
+    onChange,
+    options,
+    delimiter,
+    validationErrorMsgStyle,
+    placeholder = "Select an option",
+    ...rest
+  } = props;
+
+  let errorInputColor = selectErrorColor(errorMessage, touched)
+
+  // // Redux State, for UI updates
+  // // Keep separate from Redux updates, which have different data structure
+  const [state, setState] = React.useState(props.initialState)
+  const snackbar = useSnackbar()
+
+
+  return (
+    <div className={clsx(
+      classes.root,
+      classes.creatableSelect,
+      props.className && props.className,
+    )}>
+      <Select
+        // inputId={props.inputId}
+        value={state}
+        onMenuOpen={props.onMenuOpen}
+        onChange={(e) => {
+          // console.log('e: "', e)
+          if (e?.length > props.itemLimit) {
+            snackbar.enqueueSnackbar(
+              `Cannot choose more than ${props.itemLimit}`,
+              { variant: 'info'}
+            )
+            // setState(e.slice(0, props.itemLimit))
+            // onChange(e.slice(0, props.itemLimit))
+            return
+          }
+          setState(e)
+          onChange(e)
+        }}
+        menuIsOpen={props.menuIsOpen}
+        menuPlacement={props.menuPlacement ?? "auto" }
+        options={options}
+        placeholder={placeholder}
+        isMulti={isMulti}
+        isSearchable={isSearchable}
+        isClearable={isClearable}
+        isLoading={props.loading}
+        menuPortalTarget={props.menuPortalTarget}
+        autoComplete={
+          props.disableAutocomplete
+          ? "new-password"
+          : undefined
+        } // this disables autofill
+        className={classes.optionValues}
+        classes={{
+          input: clsx(
+            classes.input,
+            errorInputColor === "red" ? classes.errorInput : null,
+            errorInputColor === "grey" ? classes.errorInputUntouched : null,
+          ),
+          multiline: classes.selectMultiline,
+        }}
+        components={props.components}
+        theme={
+          props.theme
+          ? props.theme
+          : theme => ({
+              ...theme,
+              borderRadius: BorderRadius,
+              colors: {
+                ...theme.colors,
+                color: isDarkMode
+                  ? Colors.uniswapLightestGrey
+                  : Colors.black,
+                primary25: isDarkMode
+                  ? Colors.uniswapLightNavy
+                  : Colors.slateGreyDarker,
+                primary: Colors.uniswapLighterGrey,
+              },
+            })
+        }
+        styles={
+          props.styles
+          ? props.styles
+          : {
+              input: styles => ({
+                color: props.hideCursor
+                  ? 'transparent'
+                  : isThemeDark(theme)
+                    ? Colors.uniswapLightGrey
+                    : Colors.slateGreyBlack
+              }),
+              placeholder: styles => ({
+                ...styles,
+                fontWeight: 400,
+                fontFamily: fontFam,
+                whiteSpace: 'nowrap',
+                color: Colors.grey,
+              }),
+              singleValue: styles => ({
+                ...styles,
+                color: isDarkMode ? Colors.uniswapLighterGrey : Colors.charcoal,
+                overflow: 'hidden',
+                whiteSpace: 'nowrap',
+                textOverflow: 'ellipsis',
+                maxWidth: '250px',
+              }),
+              multiValue: styles => ({
+                ...styles,
+                color: isDarkMode ? Colors.cream : Colors.cream,
+                backgroundColor: isDarkMode ? Colors.purple : Colors.blue,
+              }),
+              multiValueLabel: styles => ({
+                ...styles,
+                fontWeight: 500,
+                color: isDarkMode ? Colors.cream : Colors.cream,
+              }),
+              multiValueRemove: styles => ({
+                ...styles,
+                cursor: "pointer",
+              }),
+              indicatorSeparator: styles => ({
+                ...styles,
+                backgroundColor: isDarkMode ? Colors.uniswapLighterGrey : Colors.slateGreyDarker,
+                display: props.hideButton ? "none" : "inherit",
+              }),
+              indicatorsContainer: styles => ({
+                ...styles,
+                color: isDarkMode ? Colors.uniswapLighterGrey : Colors.charcoal,
+                cursor: "pointer",
+                display: props.hideButton ? "none" : "inherit",
+              }),
+              container: styles => ({
+                ...styles,
+                height: height,
+                borderRadius: BorderRadius,
+              }),
+              menu: styles => ({
+                ...styles,
+                zIndex: 10,
+                marginTop: '2px',
+                cursor: "pointer",
+                "&:hover": {
+                  cursor: "pointer",
+                },
+                color: isDarkMode ? Colors.uniswapLightestGrey : Colors.charcoal,
+                background: isDarkMode
+                  ? Colors.uniswapMediumNavy
+                  : Colors.slateGrey,
+              }),
+              menuList: styles => ({
+                ...styles,
+                maxHeight: '315px',
+              }),
+              control: (base, state) => ({
+                ...base,
+                background: isDarkMode
+                  ? Colors.uniswapMediumNavy
+                  : Colors.slateGrey,
+                // // match with the menu
+                cursor: "text",
+                borderRadius: BorderRadius,
+                height: '100%',
+                border: isDarkMode
+                  ? `1px solid ${Colors.uniswapMediumGrey}`
+                  : `1px solid ${Colors.slateGreyDarker}`,
+              }),
+          }
+        }
+        inputRef={ref}
+      />
+
+    </div>
+  )
+}
+
+const selectErrorColor = (
+  errorMessage: string,
+  touched: boolean,
+) => {
+  if (!touched) {
+    return "grey"
+  }
+  if (!errorMessage) {
+    return "none"
+  }
+  if (errorMessage && touched) {
+    return "red"
+  } else {
+    return "none"
+  }
+}
+
+
+interface ReactProps extends WithStyles<typeof styles> {
+  errorMessage?: string;
+  touched?: boolean; // sets error colors as grey if not-touched, red if so
+  initialState: any;
+  onChange(...args: any): void;
+  onMenuOpen(...args: any): void;
+  isMulti?: boolean;
+  limit?: { count: number, max: number };
+  options: SelectOption[];
+  placeholder: string;
+  delimiter?: string;
+  disableInitialValidationMessage?: boolean;
+  inputId?: string;
+  className?: any;
+  isSearchable?: boolean;
+  isClearable?: boolean;
+  hideCursor?: boolean;
+  styles?: any;
+  theme?: any;
+  height?: any;
+  loading?: boolean;
+  disableAutocomplete?: boolean;
+  menuPortalTarget?: any;
+  menuIsOpen?: any;
+  menuPlacement?: "bottom" | "top" | "auto"
+  hideButton?: boolean;
+  components?: any;
+  itemLimit?: number;
+  [key: string]: any;
+}
+export interface SelectOption {
+  label: string;
+  value: string | any;
+}
+
+
+export default withStyles(styles)( CaliberDropdown );
